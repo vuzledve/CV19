249357cce7b279e512601208a7603cdba8b94edd
Дмитрий Б <vuzledve@mail.ru>
2022-03-24 14:32:41 +0300

    Базовый класс команды

---
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal class Command : ICommand
+    {
+        //активируется когда CanExecute начинает возвращать другое значение
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value; 
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        //может ли команда быть выполнена 
+        public abstract bool CanExecute(object? parameter)
+       
+
+        //то, что должно быть выполнено командой (логика команды)
+        public abstract void Execute(object? parameter)
+        
+    }
+}
