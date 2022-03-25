18f2e5de02c3075fc0b75ee792e8e5f2d1f9d510
Дмитрий Б <vuzledve@mail.ru>
2022-03-25 19:25:29 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+namespace CV19Console
+{
+    class Program
+    {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine("hello!");
+        }
+    }
+}
