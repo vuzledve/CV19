e452e99eb1ab548d394de2f2c4c9aeb3682599a0
Дмитрий Б <vuzledve@mail.ru>
2022-03-27 07:28:01 +0300

    Добавил тестовые данные для графика в модель-представление

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Тестовый набор данных для визуализации графиков
+
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        /// <summary> Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;        
+            set => Set(ref _TestDataPoints, value);
+        }
+        #endregion
+
         #region Заголовок окна
 
         private string _Title = "Анализ статистики CV19";
@@ -64,6 +77,15 @@ namespace CV19.ViewModels
             CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted,CanCloseApplicationCommandExecute);
 
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360/0.1));
+            for (var x =  0d; x <= 360; x+=0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(2 * Math.PI * x * to_rad);
+            }
+
+            TestDataPoints = data_points;
         }
 }
 }
