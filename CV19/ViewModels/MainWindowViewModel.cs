52b1d888a8971a6ee2b4b470292f8ac8fe1c60c5
Дмитрий Б <vuzledve@mail.ru>
2022-03-27 08:53:39 +0300

    тестовый график

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -9,10 +9,19 @@ using CV19.Infrastructure.Commands;
 using CV19.Models;
 using CV19.ViewModels.Base;
 
+
+
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+
+   
+      
+           
+     
+      
+
         #region Тестовый набор данных для визуализации графиков
 
         private IEnumerable<DataPoint> _TestDataPoints;
@@ -78,11 +87,13 @@ namespace CV19.ViewModels
 
             #endregion
 
-            var data_points = new List<DataPoint>((int)(360/0.1));
-            for (var x =  0d; x <= 360; x+=0.1)
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x += 0.1)
             {
                 const double to_rad = Math.PI / 180;
-                var y = Math.Sin(2 * Math.PI * x * to_rad);
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
             }
 
             TestDataPoints = data_points;
---
+++ CV19/ViewModels/PlotViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19.Models;
+using CV19.ViewModels.Base;
+
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace CV19.ViewModels
+{
+    internal class PlotViewModel : ViewModel
+    {
+        public PlotModel MyModel { get; private set; }
+
+        public PlotViewModel()
+        {
+           
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+        }
+    }
+}
