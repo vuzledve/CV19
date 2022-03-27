a2f5821ae840ac41b02b7e0266dc3de1618e1c6f
Дмитрий Б <vuzledve@mail.ru>
2022-03-27 07:17:40 +0300

    Добавил классы в Models

---
+++ CV19/Models/DataPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace CV19.Models
+{
+    internal struct DataPoint
+    {
+        public double XValue { get; set; }
+        public double YValue { get; set; }
+    }
+}
---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+}
