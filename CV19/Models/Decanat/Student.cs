6705b90113ffca72967dedf56ba267090f561c3b
Дмитрий Б <vuzledve@mail.ru>
2022-03-28 19:59:33 +0300

    Настройка дизайна DataGrid

--- CV19/Models/Decanat/Student.cs
+++ CV19/Models/Decanat/Student.cs
@@ -16,6 +16,8 @@ namespace CV19.Models.Decanat
         public DateTime Birthday { get; set; }
 
         public double Rating { get; set; }
+        
+        public string Description { get; set; }
     }
 
     internal class Group
@@ -23,5 +25,7 @@ namespace CV19.Models.Decanat
         public string Name { get; set; }
 
         public ICollection<Student> Students { get; set; }
+
+        public string Description { get; set; }
     }
 }
