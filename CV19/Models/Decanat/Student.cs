2af92ad6125d5f7f857d610d6476105ae5bcf884
Дмитрий Б <vuzledve@mail.ru>
2022-03-28 14:33:44 +0300

    Тестовая предметная область

---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+        
+        public DateTime Birthday { get; set; }
+
+        public double Rating { get; set; }
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ CV19/ViewModels/Lesson_2/Lesson_2ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels.Lesson_2
+{
+    internal class Lesson_2ViewModel : ViewModel
+    {
+
+        public Lesson_2ViewModel()
+        {
+
+        }
+    }
+}
