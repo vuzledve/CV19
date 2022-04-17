0a8c6bada00472d4fe5efa9c3961a95590c33076
Дмитрий Б <vuzledve@mail.ru>
2022-04-17 20:13:07 +0300

    Реализация фильтрации групп студентов в разметке. Реализация кода файла обработчиков событий окна

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.Models.Decanat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,32 @@ namespace CV19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CollectionViewSource_Filter(object sender, FilterEventArgs e)
+        {
+          
+        }
+
+        private void OnGroupNameFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
+        }
+
+        private void GroupsCollectionFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+            var filter_text = GroupNameFilterText.Text;
+            if (filter_text.Length == 0) return;
+
+            if (group.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (group.Description != null && group.Description.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
         }
     }
 }
