72289f34b6502bfce527a0556526889389214875
Дмитрий Б <vuzledve@mail.ru>
2022-04-18 04:36:47 +0300

    Разметка для тестовых данных для демонстрации потребления памяти визуальной частью

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -13,5 +13,12 @@ namespace CV19
     /// </summary>
     public partial class App : Application
     {
+        public static bool IsDesignMode { get; private set; } = true;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+            base.OnStartup(e);
+        }
     }
 }
--- CV19/ViewModels/Lesson_2/Lesson2ViewModel.cs
+++ CV19/ViewModels/Lesson_2/Lesson2ViewModel.cs
@@ -87,8 +87,15 @@ namespace CV19.ViewModels.Lesson_2
         }
         #endregion
 
+
         #endregion
 
+        public IEnumerable<Student> TestStudents => Enumerable.Range(1, App.IsDesignMode ? 10 : 100_000)
+            .Select(i => new Student
+            {
+                Name = $"Имя {i}",
+                Surname = $"Фамилия {i}"
+            });
         public Lesson2ViewModel()
         {
             DeleteGroupCommand = new ActionCommand(OnDeleteGroupCommandExecuted, CanDeleteGroupCommandExecute);
