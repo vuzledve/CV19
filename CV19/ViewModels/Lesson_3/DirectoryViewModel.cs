344db84d065fc8b11422bdbdb09901f74a23c9cb
Дмитрий Б <vuzledve@mail.ru>
2022-04-20 06:10:17 +0300

    Применение шаблона отображения иерархических данных в визуальном элементе TreeView

---
+++ CV19/ViewModels/Lesson_3/DirectoryViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.ViewModels.Base;
+using CV19.Models.Decanat;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows.Data;
+using System.ComponentModel;
+using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace CV19.ViewModels.Lesson_3
+{
+    internal class DirectoryViewModel : ViewModel
+    {
+       
+
+        private readonly DirectoryInfo _DirectoryInfo; //Статический класс Directory предоставляет ряд методов для управления каталогами
+
+        public IEnumerable<DirectoryViewModel> SubDirectories
+        {
+            get
+            {
+                try
+                {
+                    return _DirectoryInfo
+                       .EnumerateDirectories()
+                       .Select(dir_info => new DirectoryViewModel(dir_info.FullName));
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Debug.WriteLine(e.ToString());
+                }
+                return Enumerable.Empty<DirectoryViewModel>();
+            }
+        }
+       
+
+        public IEnumerable<FileViewModel> Files
+        {
+            get
+            {
+                try
+                {
+                    var files = _DirectoryInfo
+                               .EnumerateFiles()
+                               .Select(file_info => new FileViewModel(file_info.FullName));
+                    return files;
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Debug.WriteLine(e.ToString());
+                }
+                return Enumerable.Empty<FileViewModel>();
+            }
+        }
+        
+        public IEnumerable<object> DirectoryItems
+        {
+            get
+            {
+                try
+                {
+                    var items =  SubDirectories
+                        .Cast<object>() //Cast - используется для приведения каждого элемента входной последовательности в выходную последовательность указанного типа
+                        .Concat(Files); //Concat - Объединяет один или несколько экземпляров класса
+                    return items;
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Debug.WriteLine(e.ToString());
+                }
+                return Enumerable.Empty<object>();
+            }
+        }
+       
+
+        public string Name => _DirectoryInfo.Name;
+        public string Path => _DirectoryInfo.FullName;
+        public DateTime CreationTime => _DirectoryInfo.CreationTime;
+
+        public DirectoryViewModel(string Path) => _DirectoryInfo= new DirectoryInfo(Path);
+
+
+
+    }
+
+    class FileViewModel : ViewModel
+    {
+        private FileInfo _FileInfo;
+
+        public string Name => _FileInfo.Name;
+        public string Path => _FileInfo.FullName;
+        public DateTime CreationTime => _FileInfo.CreationTime;
+
+        public FileViewModel(string Path) => _FileInfo = new FileInfo(Path);
+        
+    }
+}
---
+++ CV19/ViewModels/Lesson_3/Lesson3ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.ViewModels.Base;
+using CV19.ViewModels;
+using CV19.Models.Decanat;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows.Data;
+using System.ComponentModel;
+using System;
+
+namespace CV19.ViewModels.Lesson_3
+{
+    internal class Lesson3ViewModel : ViewModel
+    {
+        public DirectoryViewModel DiskRootDir { get; } = new DirectoryViewModel("c:\\");
+
+
+
+        #region SelectedDirectory : DirectoryViewModel - Выбранная директория
+
+        /// <summary>Выбранная директория</summary>
+        private DirectoryViewModel _SelectedDirectory;
+
+        /// <summary>Выбранная директория</summary>
+        public DirectoryViewModel SelectedDirectory
+        {
+            get => _SelectedDirectory;
+            set => Set(ref _SelectedDirectory, value);
+        }
+
+        #endregion
+
+
+        public Lesson3ViewModel()
+        {
+
+        }
+
+    }
+}
